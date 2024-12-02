468e0c8d4fd7349ad09b4a35b038e75f9ef306b9
maharrington42 <102279849+Olhado333@users.noreply.github.com>
2024-12-01 23:29:10 -0600

    Added parse_input()

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,4 +2,25 @@ use std::fs;
 
 fn main() {
     let puzzle_input = fs::read_to_string("src/puzzle_input.txt").expect("Failed to read puzzle input.");
+
+    let reports = parse_input(&puzzle_input);
+
+    for report in reports {
+        println!("{report:?}");
+    }
+}
+
+fn parse_input(puzzle_input: &str) -> Vec<Vec<i32>> {
+    let lines = puzzle_input.lines().collect::<Vec<&str>>();
+
+    lines
+        .into_iter()
+        .map(|line| {
+            line.split(" ")
+                .map(|num|
+                    num.parse::<i32>()
+                    .expect("Failed to convert &str to i32."))
+                    .collect::<Vec<_>>()
+        })
+        .collect::<Vec<Vec<_>>>()
 }
