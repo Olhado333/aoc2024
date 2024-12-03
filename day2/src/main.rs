ec9951838991911a587ae9336ed6de20c6d3e8c4
maharrington42 <102279849+Olhado333@users.noreply.github.com>
2024-12-02 20:07:15 -0600

    remove problem_dampener() and refactored is_report_safe()

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,8 +1,8 @@
-use std::fs;
+use std::{collections::btree_set::Difference, fs};
 
 fn main() {
     let puzzle_input =
-        fs::read_to_string("src/mini_puzzle_input.txt").expect("Failed to read puzzle input.");
+        fs::read_to_string("src/puzzle_input.txt").expect("Failed to read puzzle input.");
 
     let reports = parse_input(&puzzle_input);
 
@@ -11,70 +11,58 @@ fn main() {
 
 fn num_safe_reports(reports: &Vec<Vec<i32>>) -> u32 {
     let mut count = 0;
+    let mut balls = 0;
 
     for report in reports {
+        println!("\n{balls}:");
+        balls += 1;
         if is_report_safe(report) {
             count += 1;
-            println!("safe");
         }
     }
 
     count
 }
 
-fn problem_dampener(mut report: Vec<i32>) -> Vec<i32> {
-    let mut first_pointer = 0;
-    let mut second_pointer = 1;
-
-    let increasing = report[first_pointer] < report[second_pointer];
-
-    for _ in 0..(report.len() - 1) {
-        if (report[first_pointer] < report[second_pointer]) != increasing {
-            report.remove(first_pointer);
-            break;
-        }
-
-        let difference = report[first_pointer].abs_diff(report[second_pointer]);
-
-        if (difference < 1) || (difference > 3) {
-            report.remove(first_pointer);
-            break;
-        }
-
-        first_pointer += 1;
-        second_pointer += 1;
-    }
-
-    report
-}
-
 fn is_report_safe(report: &Vec<i32>) -> bool {
-    println!("Before removing problematic element:\n{report:?}");
-
-    let report = problem_dampener(report.clone());
-
-    println!("After:\n{report:?}");
-
-    if report.len() < 2 {
-        return true;
-    }
+    let mut is_increasing = report[0] < report[1];
+    let mut problem_dampener = true;
 
     let mut first_pointer = 0;
     let mut second_pointer = 1;
-    let increasing = report[first_pointer] < report[second_pointer];
+
+    println!("{report:?}");
 
     for _ in 0..(report.len() - 1) {
-        if (report[first_pointer] < report[second_pointer]) != increasing {
-            return false;
+        let first = report[first_pointer];
+        let second = report[second_pointer];
+        let current_is_increasing = first < second;
+
+        if current_is_increasing != is_increasing {
+            if !problem_dampener { return false; }
+            problem_dampener = false;
+
+            second_pointer += 1;
+            continue
+        } else {
+            is_increasing = current_is_increasing;
         }
 
-        let difference = report[first_pointer].abs_diff(report[second_pointer]);
+        let difference = first.abs_diff(second);
 
         if (difference < 1) || (difference > 3) {
-            return false;
+            if !problem_dampener { return false; }
+            problem_dampener = false;
+
+            second_pointer += 1;
+            continue;
         }
 
-        first_pointer += 1;
+        println!("first: {}, second: {}", first, second);
+
+        println!("{problem_dampener}");
+
+        first_pointer = second_pointer;
         second_pointer += 1;
     }
 
