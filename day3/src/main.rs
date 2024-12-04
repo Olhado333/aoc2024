cc2e49756a8c5cd0c7db2c830ad286fe625dec42
maharrington42 <102279849+Olhado333@users.noreply.github.com>
2024-12-04 14:18:17 -0600

    Reads puzzle_input.txt to string

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input = fs::read_to_string("src/puzzle_input.txt").expect("Failed to read in puzzle input.");
+    println!("{puzzle_input}");
 }
