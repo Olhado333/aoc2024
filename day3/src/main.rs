5bd24e9f0b86d493d1e4d44163a8163d734be969
maharrington42 <102279849+Olhado333@users.noreply.github.com>
2024-12-16 16:08:23 -0600

    added small_puzzle_input.txt

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,5 +2,10 @@ use std::fs;
 
 fn main() {
     let puzzle_input = fs::read_to_string("src/puzzle_input.txt").expect("Failed to read in puzzle input.");
-    println!("{puzzle_input}");
 }
+
+fn part_one(puzzle_input: String) -> u32 {
+    
+}
+
+
