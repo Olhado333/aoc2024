8c313bf7dce32ebc4fb14f9b197b424f9bd1b998
maharrington42 <102279849+Olhado333@users.noreply.github.com>
2024-12-16 17:29:47 -0600

    adds part_one()

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,11 +1,25 @@
 use std::fs;
+use regex::Regex;
 
 fn main() {
-    let puzzle_input = fs::read_to_string("src/puzzle_input.txt").expect("Failed to read in puzzle input.");
+    let puzzle_input = fs::read_to_string("src/small_puzzle_input.txt").unwrap();
+    
+    let answer = part_one(&puzzle_input);
+    println!("{answer}");
 }
 
-fn part_one(puzzle_input: String) -> u32 {
+fn part_one(puzzle_input: &str) -> u32 {
+    let pattern = Regex::new(r"mul\((?<first>[0-9]{1,3}),(?<second>[0-9]{1,3})\)").unwrap();
+    let mut sum = 0;
+
+    for (_, [first, second]) in pattern.captures_iter(&puzzle_input).map(|c| c.extract()) {
+        let num1 = first.parse::<u32>().unwrap();
+        let num2 = second.parse::<u32>().unwrap();
+
+        sum += num1 * num2;
+    }
     
+    sum
 }
 
 
